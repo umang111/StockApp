dd51666584b049ba84466497d6a18acb086eb812
umang garg <umang.garg@zensar.com>
2022-05-25 17:32:17 +0530

    from office laptop

---
+++ stockApp/src/test/java/com/example/stockApp/StockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
